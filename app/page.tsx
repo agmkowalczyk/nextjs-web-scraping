76d23c29b936dd4565fd2090dc9f7b8f864ddbc9
agmkowalczyk <agm.kowalczyk@gmail.com>
2023-12-30 15:36:41 +0100

    Add Searchbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import HeroCarousel from '@/components/HeroCarousel'
+import Searchbar from '@/components/Searchbar'
 import Image from 'next/image'
 
 const Home = () => {
   return (
     <>
-      <section className='px-6 border-2 border-red-500 md:px-20 py-24'>
-        <div className='flex- max-xl:flex-col gap-16'>
+      <section className='px-6 md:px-20 py-24'>
+        <div className='flex max-xl:flex-col gap-16'>
           <div className='flex flex-col justify-center'>
             <p className='small-text'>
               Smart Shopping Starts Here:
@@ -23,9 +25,11 @@ const Home = () => {
               Powerful, self-serve product and growth analytics to help you
               convert, engage and retain more.
             </p>
-            Searchbar
+
+            <Searchbar />
           </div>
-          HeroCarousel
+
+          <HeroCarousel />
         </div>
       </section>
 
@@ -34,7 +38,7 @@ const Home = () => {
 
         <div className='flex flex-wrap gap-x-8 gap-y-16'>
           {['aa', 'bb'].map((product) => (
-            <div>{product}</div>
+            <div key={product}>{product}</div>
           ))}
         </div>
       </section>
